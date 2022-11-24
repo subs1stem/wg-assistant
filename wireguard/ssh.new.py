a1f6a11f802947f251be7ab8996362228184f984
subs1stem <subs1stem@yandex.ru>
2022-11-25 01:23:10 +0300

    Working on new ssh class

--- wireguard/ssh.new.py
+++ wireguard/ssh.new.py
@@ -8,6 +8,7 @@ from settings import *
 
 class SSH:
     path_to_tmp_config = '/tmp/wg0.conf'
+    wg_config = WGConfig(path_to_tmp_config)
 
     def __init__(self):
         self.host = SSH_HOST
@@ -57,44 +58,39 @@ class SSH:
         self.client.open_sftp().put(self.path_to_tmp_config, self.path_to_config)
 
     def add_peer(self, peer_name):
-        pass
+        self.download_config()
+        self.wg_config.read_file()
 
     def delete_peer(self, pubkey):
         self.download_config()
-        wg_config = WGConfig(self.path_to_tmp_config)
-        wg_config.read_file()
-        wg_config.del_peer(pubkey)
-        wg_config.write_file()
+        self.wg_config.read_file()
+        self.wg_config.del_peer(pubkey)
+        self.wg_config.write_file()
         self.upload_config()
         self.wg_down_up()
 
     def enable_peer(self, pubkey):
         self.download_config()
-        wg_config = WGConfig(self.path_to_tmp_config)
-        wg_config.read_file()
-        wg_config.enable_peer(pubkey)
-        wg_config.write_file()
+        self.wg_config.read_file()
+        self.wg_config.enable_peer(pubkey)
+        self.wg_config.write_file()
         self.upload_config()
         self.wg_down_up()
 
     def disable_peer(self, pubkey):
         self.download_config()
-        wg_config = WGConfig(self.path_to_tmp_config)
-        wg_config.read_file()
-        wg_config.disable_peer(pubkey)
-        wg_config.write_file()
+        self.wg_config.read_file()
+        self.wg_config.disable_peer(pubkey)
+        self.wg_config.write_file()
         self.upload_config()
         self.wg_down_up()
 
     def get_peer_enabled(self, pubkey):
         self.download_config()
-        wg_config = WGConfig(self.path_to_tmp_config)
-        wg_config.read_file()
-        wg_config.get_peer_enabled(pubkey)
-        wg_config.write_file()
-        self.upload_config()
-        self.wg_down_up()
+        self.wg_config.read_file()
+        return self.wg_config.get_peer_enabled(pubkey)
 
 
 if __name__ == '__main__':
-    print(SSH().get_raw_config())
+    ssh = SSH()
+    ssh.delete_peer('U0cwkQJMZlUPtNQ7n1FpXoAQZ5rbCJ6gzUuW+9yqSy0=')
