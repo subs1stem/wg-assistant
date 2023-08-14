0f4d7189056f7a7ecaeaffaad5c93834c2f13ecd
subs1stem <subs1stem@yandex.ru>
2023-08-14 23:01:36 +0300

    WIP: migration to aiogram v3.0

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -1,4 +1,4 @@
-from aiogram import types
+from aiogram import types, Router
 from aiogram.enums import ParseMode
 from aiogram.fsm.context import FSMContext
 
@@ -7,116 +7,127 @@ from modules.keyboards import *
 from modules.messages import peers_message
 from wireguard.ssh import SSH
 
+router = Router()
 
-async def set_callback_handlers(dp):
-    @dp.callback_query_handler(lambda query: str(query.from_user.id) not in environ['ADMIN_ID'].split(','))
-    async def send_access_warning(query: types.CallbackQuery):
-        await query.answer()
-        await dp.bot.send_message(chat_id=query.from_user.id,
+
+@router.callback_query(lambda callback: str(callback.from_user.id) not in environ['ADMIN_ID'].split(','))
+async def access_warning(callback: types.CallbackQuery):
+    await callback.message.answer(chat_id=callback.from_user.id,
                                   text='Доступ запрещён 🛑')
 
-    @dp.callback_query_handler(lambda query: query.data == 'main_menu', state='*')
-    async def send_main_menu(query: types.CallbackQuery, state: FSMContext):
-        await query.answer()
-        await state.clear()
-        await dp.bot.edit_message_text(chat_id=query.from_user.id,
-                                       message_id=query.message.message_id,
-                                       text='Главное меню:',
-                                       reply_markup=main_menu_keyboard())
 
-    @dp.callback_query_handler(lambda query: query.data == 'wg_options')
-    async def send_wg_options(query: types.CallbackQuery):
-        await query.answer('Загрузка...')
-        await dp.bot.edit_message_text(chat_id=query.from_user.id,
-                                       message_id=query.message.message_id,
-                                       text='Параметры WireGuard:',
-                                       reply_markup=wg_options_keyboard())
+@router.callback_query(lambda callback: callback.data == 'main_menu', state='*')
+async def main_menu(callback: types.CallbackQuery, state: FSMContext):
+    await state.clear()
+    await callback.bot.edit_message_text(chat_id=callback.from_user.id,
+                                         message_id=callback.message.message_id,
+                                         text='Главное меню:',
+                                         reply_markup=main_menu_keyboard())
 
-    @dp.callback_query_handler(lambda query: query.data == 'reboot_server')
-    async def reboot_server(query: types.CallbackQuery):
-        await query.answer('Перезагружаю сервер...')
-        SSH().reboot()
 
-    @dp.callback_query_handler(lambda query: query.data == 'get_peers')
-    async def send_peers(query: types.CallbackQuery):
-        await query.answer('Запрашиваю список пиров...')
-        await dp.bot.edit_message_text(chat_id=query.from_user.id,
-                                       message_id=query.message.message_id,
-                                       text=f'{peers_message()}',
-                                       parse_mode=ParseMode.HTML,
-                                       reply_markup=peer_list_keyboard())
+@router.callback_query(lambda callback: callback.data == 'wg_options')
+async def wg_options(callback: types.CallbackQuery):
+    await callback.answer('Загрузка...')
+    await callback.bot.edit_message_text(chat_id=callback.from_user.id,
+                                         message_id=callback.message.message_id,
+                                         text='Параметры WireGuard:',
+                                         reply_markup=wg_options_keyboard())
 
-    @dp.callback_query_handler(lambda query: query.data == 'get_server_config')
-    async def send_raw_config(query: types.CallbackQuery):
-        await query.answer('Запрашиваю конфиг...')
-        await dp.bot.edit_message_text(chat_id=query.from_user.id,
-                                       message_id=query.message.message_id,
-                                       text=f'<code>{SSH().get_raw_config()}</code>',
-                                       parse_mode=ParseMode.HTML,
-                                       reply_markup=back_button('wg_options'))
 
-    @dp.callback_query_handler(lambda query: query.data.startswith('wg_state'))
-    async def change_wg_state(query: types.CallbackQuery):
-        await query.answer('Выполняю...')
-        state = query.data.split('_')[-1]
-        SSH().wg_change_state(state)
-        await dp.bot.edit_message_reply_markup(chat_id=query.from_user.id,
-                                               message_id=query.message.message_id,
-                                               reply_markup=wg_options_keyboard())
+@router.callback_query(lambda callback: callback.data == 'reboot_server')
+async def reboot_server(callback: types.CallbackQuery):
+    await callback.answer('Перезагружаю сервер...')
+    SSH().reboot()
 
-    @dp.callback_query_handler(lambda query: query.data == 'add_peer')
-    async def add_peer(query: types.CallbackQuery):
-        await query.answer()
-        await dp.bot.edit_message_text(chat_id=query.from_user.id,
-                                       message_id=query.message.message_id,
-                                       text='Пришли мне имя клиента',
-                                       reply_markup=cancel_button('main_menu'))
-        await AddPeer.waiting_for_peer_name.set()
 
-    @dp.callback_query_handler(lambda query: query.data == 'config_peers')
-    async def config_peers(query: types.CallbackQuery):
-        await query.answer('Запрашиваю список пиров...')
-        await dp.bot.edit_message_text(chat_id=query.from_user.id,
-                                       message_id=query.message.message_id,
-                                       text='Выбери клиента:',
-                                       reply_markup=peers_keyboard())
+@router.callback_query(lambda callback: callback.data == 'get_peers')
+async def peers(callback: types.CallbackQuery):
+    await callback.answer('Запрашиваю список пиров...')
+    await callback.bot.edit_message_text(chat_id=callback.from_user.id,
+                                         message_id=callback.message.message_id,
+                                         text=f'{peers_message()}',
+                                         parse_mode=ParseMode.HTML,
+                                         reply_markup=peer_list_keyboard())
 
-    @dp.callback_query_handler(lambda query: query.data.startswith('peer'))
-    async def show_peer(query: types.CallbackQuery):
-        await query.answer()
-        _, pubkey = query.data.split(':')
-        await dp.bot.edit_message_text(chat_id=query.from_user.id,
-                                       message_id=query.message.message_id,
-                                       text=f'Выбери действие:',
-                                       reply_markup=peer_action(pubkey),
-                                       parse_mode=ParseMode.HTML)
 
-    @dp.callback_query_handler(lambda query: query.data.startswith('off_peer'))
-    async def off_peer(query: types.CallbackQuery):
-        await query.answer('Отключаю...')
-        pubkey = query.data.split(':')[1]
-        SSH().disable_peer(pubkey)
-        await dp.bot.edit_message_text(chat_id=query.from_user.id,
-                                       message_id=query.message.message_id,
-                                       text='Выбери клиента:',
-                                       reply_markup=peers_keyboard())
+@router.callback_query(lambda callback: callback.data == 'get_server_config')
+async def raw_config(callback: types.CallbackQuery):
+    await callback.answer('Запрашиваю конфиг...')
+    await callback.bot.edit_message_text(chat_id=callback.from_user.id,
+                                         message_id=callback.message.message_id,
+                                         text=f'<code>{SSH().get_raw_config()}</code>',
+                                         parse_mode=ParseMode.HTML,
+                                         reply_markup=back_button('wg_options'))
 
-    @dp.callback_query_handler(lambda query: query.data.startswith('on_peer'))
-    async def on_peer(query: types.CallbackQuery):
-        await query.answer('Включаю...')
-        pubkey = query.data.split(':')[1]
-        SSH().enable_peer(pubkey)
-        await dp.bot.edit_message_text(chat_id=query.from_user.id,
-                                       message_id=query.message.message_id,
-                                       text='Выбери клиента:',
-                                       reply_markup=peers_keyboard())
 
-    @dp.callback_query_handler(lambda query: query.data.startswith('del_peer'))
-    async def del_peer(query: types.CallbackQuery):
-        await query.answer('Удаляю...')
-        pubkey = query.data.split(':')[1]
-        SSH().delete_peer(pubkey)
-        await dp.bot.edit_message_text(chat_id=query.from_user.id,
-                                       message_id=query.message.message_id,
-                                       text='Выбери клиента:',
-                                       reply_markup=peers_keyboard())
+@router.callback_query(lambda callback: callback.data.startswith('wg_state'))
+async def change_wg_state(callback: types.CallbackQuery):
+    await callback.answer('Выполняю...')
+    state = callback.data.split('_')[-1]
+    SSH().wg_change_state(state)
+    await callback.bot.edit_message_reply_markup(chat_id=callback.from_user.id,
+                                                 message_id=callback.message.message_id,
+                                                 reply_markup=wg_options_keyboard())
+
+
+@router.callback_query(lambda callback: callback.data == 'add_peer')
+async def add_peer(callback: types.CallbackQuery):
+    await callback.answer()
+    await callback.bot.edit_message_text(chat_id=callback.from_user.id,
+                                         message_id=callback.message.message_id,
+                                         text='Пришли мне имя клиента',
+                                         reply_markup=cancel_button('main_menu'))
+    await AddPeer.waiting_for_peer_name.set()
+
+
+@router.callback_query(lambda callback: callback.data == 'config_peers')
+async def config_peers(callback: types.CallbackQuery):
+    await callback.answer('Запрашиваю список пиров...')
+    await callback.bot.edit_message_text(chat_id=callback.from_user.id,
+                                         message_id=callback.message.message_id,
+                                         text='Выбери клиента:',
+                                         reply_markup=peers_keyboard())
+
+
+@router.callback_query(lambda callback: callback.data.startswith('peer'))
+async def show_peer(callback: types.CallbackQuery):
+    await callback.answer()
+    _, pubkey = callback.data.split(':')
+    await callback.bot.edit_message_text(chat_id=callback.from_user.id,
+                                         message_id=callback.message.message_id,
+                                         text=f'Выбери действие:',
+                                         reply_markup=peer_action(pubkey),
+                                         parse_mode=ParseMode.HTML)
+
+
+@router.callback_query(lambda callback: callback.data.startswith('off_peer'))
+async def off_peer(callback: types.CallbackQuery):
+    await callback.answer('Отключаю...')
+    pubkey = callback.data.split(':')[1]
+    SSH().disable_peer(pubkey)
+    await callback.bot.edit_message_text(chat_id=callback.from_user.id,
+                                         message_id=callback.message.message_id,
+                                         text='Выбери клиента:',
+                                         reply_markup=peers_keyboard())
+
+
+@router.callback_query(lambda callback: callback.data.startswith('on_peer'))
+async def on_peer(callback: types.CallbackQuery):
+    await callback.answer('Включаю...')
+    pubkey = callback.data.split(':')[1]
+    SSH().enable_peer(pubkey)
+    await callback.bot.edit_message_text(chat_id=callback.from_user.id,
+                                         message_id=callback.message.message_id,
+                                         text='Выбери клиента:',
+                                         reply_markup=peers_keyboard())
+
+
+@router.callback_query(lambda callback: callback.data.startswith('del_peer'))
+async def del_peer(callback: types.CallbackQuery):
+    await callback.answer('Удаляю...')
+    pubkey = callback.data.split(':')[1]
+    SSH().delete_peer(pubkey)
+    await callback.bot.edit_message_text(chat_id=callback.from_user.id,
+                                         message_id=callback.message.message_id,
+                                         text='Выбери клиента:',
+                                         reply_markup=peers_keyboard())
