884206d61378fa6233c2b17f79679e6f22f39f13
subs1stem <subs1stem@yandex.ru>
2023-09-23 19:04:01 +0300

    WIP: migration to Redis storage

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ import asyncio
 from os import environ
 
 from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import RedisStorage
 from aiogram.types import BotCommand
 from dotenv import load_dotenv
+from redis.asyncio import Redis
 
 from handlers import commands, callbacks, messages
 from modules.middlewares import AuthCheckMiddleware
 
 load_dotenv()
 
+redis = Redis(
+    host=environ['REDIS_HOST'],
+    port=int(environ['REDIS_PORT']),
+)
+
 
 async def main():
     admin_list = [int(admin_id) for admin_id in environ['ADMIN_ID'].split(',')]
     bot = Bot(token=environ['TOKEN'], parse_mode='HTML')
-    dp = Dispatcher(storage=MemoryStorage(), admin_list=admin_list)
+    dp = Dispatcher(storage=RedisStorage(redis), admin_list=admin_list)
 
     dp.update.middleware(AuthCheckMiddleware())
     # dp.update.filter(MagicData(~F.event.from_user.id.in_(admin_list)))
