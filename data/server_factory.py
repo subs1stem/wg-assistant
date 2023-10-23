a6e4f3faf73853a4148e1e77b3bc92ebea6ef1ce
subs1stem <subs1stem@yandex.ru>
2023-10-23 21:36:57 +0300

    Update server_factory, add typing and docstrings

--- data/server_factory.py
+++ data/server_factory.py
@@ -1,11 +1,24 @@
 from wireguard.linux import Linux
 from wireguard.mikrotik import MikroTik
+from wireguard.wireguard import WireGuard
 
 
-def create_server_instance(class_name, server_data):
-    if class_name == "Linux":
+def create_server_instance(class_name: str, server_data: dict) -> WireGuard:
+    """Create an instance of a WireGuard server based on the provided class name and server data.
+
+    Args:
+        class_name (str): The name of the server class to create (e.g., "Linux" or "MikroTik").
+        server_data (dict): A dictionary containing server configuration data.
+
+    Returns:
+        WireGuard: An instance of the WireGuard server.
+
+    Raises:
+        ValueError: If an unknown server class is provided.
+    """
+    if class_name == 'Linux':
         return Linux(**server_data)
-    elif class_name == "MikroTik":
+    elif class_name == 'MikroTik':
         return MikroTik(**server_data)
     else:
-        raise ValueError(f"Unknown server class: {class_name}")
+        raise ValueError(f'Unknown server class: {class_name}')
