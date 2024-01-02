bfbf125cacc80d50c12fb189ced5be897263cc78
subs1stem <subs1stem@yandex.ru>
2024-01-02 18:28:24 +0300

    Update requirements, minor changes

--- handlers/errors.py
+++ handlers/errors.py
@@ -7,10 +7,10 @@ router = Router()
 
 @router.error(ExceptionTypeFilter(ConnectionError), F.update.callback_query.as_('callback'))
 async def handle_connection_error(_, callback: CallbackQuery):
-    await callback.answer(f'Не удалось подключиться к серверу ⚠️', show_alert=True)
+    await callback.answer(f'Ошибка подключения к серверу ⚠️', show_alert=True)
 
 
 @router.error(F.update.callback_query.as_('callback'))
 async def handle_error(event: ErrorEvent, callback: CallbackQuery):
     print(event.exception)
-    await callback.answer(f'Неизвестная ошибка! ⚠️', show_alert=True)
+    await callback.answer(f'Неизвестная ошибка ⚠️', show_alert=True)
