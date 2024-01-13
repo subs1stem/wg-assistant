06004fcb16d0e583f06786a0a40321ee7d40f01c
subs1stem <subs1stem@yandex.ru>
2024-01-13 17:34:50 +0300

    Add state clearing when calling /commands and when catching errors

--- handlers/commands.py
+++ handlers/commands.py
@@ -1,5 +1,6 @@
 from aiogram import Router
 from aiogram.filters import Command, CommandStart
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
 from modules.keyboards import servers_kb
@@ -8,11 +9,13 @@ router = Router()
 
 
 @router.message(CommandStart())
-async def send_start(message: Message):
+async def send_start(message: Message, state: FSMContext):
+    await state.clear()
     await message.answer(f'Привет, {message.chat.username or "%username%"}! 👋')
 
 
 @router.message(Command('servers'))
-async def send_servers(message: Message, servers: dict):
+async def send_servers(message: Message, servers: dict, state: FSMContext):
+    await state.clear()
     server_names = list(servers.keys())
     await message.answer('Список серверов:', reply_markup=servers_kb(server_names))
--- handlers/errors.py
+++ handlers/errors.py
@@ -1,12 +1,14 @@
 from aiogram import Router, F
 from aiogram.filters import ExceptionTypeFilter
+from aiogram.fsm.context import FSMContext
 from aiogram.types import ErrorEvent, CallbackQuery
 
 router = Router()
 
 
 @router.error(ExceptionTypeFilter(ConnectionError), F.update.callback_query.as_('callback'))
-async def handle_connection_error(_, callback: CallbackQuery):
+async def handle_connection_error(_, callback: CallbackQuery, state: FSMContext):
+    await state.clear()
     await callback.answer(f'Ошибка подключения к серверу ⚠️', show_alert=True)
 
 
