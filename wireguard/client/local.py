ffc90f8e52cd459db090734e4b267ce0e8f13830
subs1stem <subs1stem@yandex.ru>
2024-12-03 00:39:43 +0300

    Implement `LocalClient`

--- wireguard/client/local.py
+++ wireguard/client/local.py
@@ -1,5 +1,19 @@
+from subprocess import Popen, PIPE
+from typing import Tuple, Any
+
 from .base import BaseClient
 
 
 class LocalClient(BaseClient):
-    pass
+    def execute(self, command: str) -> Tuple[Any, Any, Any]:
+        process = Popen(command, shell=True, stdout=PIPE, stderr=PIPE, text=True)
+        stdout, stderr = process.communicate()
+        return stdout, stderr, process.returncode
+
+    def get_file_contents(self, path: str) -> str:
+        with open(path, 'r', encoding='utf-8') as f:
+            return f.read()
+
+    def put_file_contents(self, path: str, contents: str) -> None:
+        with open(path, 'w', encoding='utf-8') as f:
+            f.write(contents)
