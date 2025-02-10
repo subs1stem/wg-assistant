dfe267e8951fe5ae9629d23f27f64d9164244311
subs1stem <subs1stem@yandex.ru>
2025-02-10 23:56:25 +0300

    Improve `Server` model types

--- models/servers.py
+++ models/servers.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, AnyUrl, IPvAnyAddress, conint
 
 
 class ServerType(str, Enum):
@@ -20,10 +20,10 @@ class Server(BaseModel):
     type: ServerType = Field(default=ServerType.LINUX, validate_default=True)
     protocol: Protocol = Field(default=Protocol.WIREGUARD, validate_default=True)
     interface_name: str | None = None
-    endpoint: str | None = None
+    endpoint: IPvAnyAddress | AnyUrl | None = None
     path_to_config: str | None = None
-    server: str | None = None
-    port: int | None = None
+    server: IPvAnyAddress | AnyUrl | None = None
+    port: conint(ge=1, le=65535) | None = None
     username: str | None = None
     password: str | None = None
     key_filename: str | None = None
