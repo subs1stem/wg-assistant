47fad65ba86b524211185e8b42d133312b686701
subs1stem <subs1stem@yandex.ru>
2025-05-02 19:14:31 +0300

    WIP: tests for `commands.py`

--- tests/test_handlers/test_commands.py
+++ tests/test_handlers/test_commands.py
@@ -1,25 +1,18 @@
-from unittest.mock import AsyncMock, MagicMock
+from unittest.mock import AsyncMock, MagicMock, patch
 
 import pytest
 
-from wg_assistant.handlers.commands import send_start
+from wg_assistant.handlers.commands import send_start, send_servers
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize(
-    'username, expected_text',
-    [
-        ('test_user', 'Hello, test_user! 👋'),
-        (None, 'Hello, %username%! 👋'),
-    ],
-)
+@pytest.mark.parametrize('username,expected_text', [
+    ('test_user', 'Hello, test_user! 👋'),
+    (None, 'Hello, %username%! 👋'),
+])
 async def test_send_start(username, expected_text):
-    message = MagicMock()
-    message.chat.username = username
-    message.answer = AsyncMock()
-
-    state = MagicMock()
-    state.clear = AsyncMock()
+    message = MagicMock(answer=AsyncMock(), chat=MagicMock(username=username))
+    state = MagicMock(clear=AsyncMock())
 
     await send_start(message, state)
 
@@ -29,7 +22,21 @@ async def test_send_start(username, expected_text):
 
 @pytest.mark.asyncio
 async def test_send_servers():
-    pass
+    message = MagicMock(answer=AsyncMock())
+    state = MagicMock(clear=AsyncMock())
+
+    server1 = MagicMock()
+    server1.name = 'server1'
+    server2 = MagicMock()
+    server2.name = 'server2'
+    servers = [server1, server2]
+
+    with patch('wg_assistant.handlers.commands.servers_kb', return_value='MOCK_KB') as kb_mock:
+        await send_servers(message, servers, state)
+
+        kb_mock.assert_called_once_with(['server1', 'server2'])
+        state.clear.assert_awaited_once()
+        message.answer.assert_awaited_once_with('Server list:', reply_markup='MOCK_KB')
 
 
 @pytest.mark.asyncio
