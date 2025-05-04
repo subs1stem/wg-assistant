f1516b4a6b53b0bb24b0b12cd7a9cec33659efe9
subs1stem <subs1stem@yandex.ru>
2025-05-04 15:41:44 +0300

    Add tests for `commands.py`

--- tests/test_handlers/test_commands.py
+++ tests/test_handlers/test_commands.py
@@ -2,7 +2,7 @@ from unittest.mock import AsyncMock, MagicMock, patch
 
 import pytest
 
-from wg_assistant.handlers.commands import send_start, send_servers
+from wg_assistant.handlers.commands import send_start, send_servers, send_settings
 
 
 @pytest.mark.asyncio
@@ -21,7 +21,8 @@ async def test_send_start(username, expected_text):
 
 
 @pytest.mark.asyncio
-async def test_send_servers():
+@patch('wg_assistant.handlers.commands.servers_kb', return_value='mock_kb')
+async def test_send_servers(mock_servers_kb):
     message = MagicMock(answer=AsyncMock())
     state = MagicMock(clear=AsyncMock())
 
@@ -31,14 +32,26 @@ async def test_send_servers():
     server2.name = 'server2'
     servers = [server1, server2]
 
-    with patch('wg_assistant.handlers.commands.servers_kb', return_value='MOCK_KB') as kb_mock:
-        await send_servers(message, servers, state)
+    await send_servers(message, servers, state)
 
-        kb_mock.assert_called_once_with(['server1', 'server2'])
-        state.clear.assert_awaited_once()
-        message.answer.assert_awaited_once_with('Server list:', reply_markup='MOCK_KB')
+    mock_servers_kb.assert_called_once_with(['server1', 'server2'])
+    state.clear.assert_awaited_once()
+    message.answer.assert_awaited_once_with('Server list:', reply_markup='mock_kb')
 
 
 @pytest.mark.asyncio
-async def test_send_settings():
-    pass
+@pytest.mark.parametrize('log_level,expected_debug_log_enabled', [
+    ('DEBUG', True),
+    ('ANY_OTHER', False),
+])
+@patch('wg_assistant.handlers.commands.bot_settings_kb', return_value='mock_kb')
+@patch('wg_assistant.handlers.commands.get_log_level')
+async def test_send_settings(mock_get_log_level, mock_bot_settings_kb, log_level, expected_debug_log_enabled):
+    message = MagicMock(answer=AsyncMock())
+    mock_get_log_level.return_value = log_level
+
+    await send_settings(message)
+
+    mock_get_log_level.assert_called_once_with()
+    mock_bot_settings_kb.assert_called_once_with(expected_debug_log_enabled)
+    message.answer.assert_awaited_once_with('Bot settings:', reply_markup='mock_kb')
