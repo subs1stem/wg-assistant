6bdba5d860cb1fb8af046abc5685236a11d12d43
subs1stem <subs1stem@yandex.ru>
2025-05-08 23:53:08 +0300

    Add test blanks for callbacks and errors handlers

---
+++ tests/test_handlers/test_callbacks.py
@@ -0,0 +1,66 @@
+import pytest
+
+
+@pytest.mark.skip
+async def test_send_servers():
+    pass
+
+
+@pytest.mark.skip
+async def test_send_server_menu():
+    pass
+
+
+@pytest.mark.skip
+async def test_send_reboot_host_confirmation():
+    pass
+
+
+@pytest.mark.skip
+async def test_reboot_host():
+    pass
+
+
+@pytest.mark.skip
+async def test_send_peer_list():
+    pass
+
+
+@pytest.mark.skip
+async def test_send_raw_config():
+    pass
+
+
+@pytest.mark.skip
+async def test_change_wg_state():
+    pass
+
+
+@pytest.mark.skip
+async def test_add_peer():
+    pass
+
+
+@pytest.mark.skip
+async def test_config_peers():
+    pass
+
+
+@pytest.mark.skip
+async def test_show_peer():
+    pass
+
+
+@pytest.mark.skip
+async def test_process_peer_action():
+    pass
+
+
+@pytest.mark.skip
+async def test_delete_peer():
+    pass
+
+
+@pytest.mark.skip
+async def test_set_debug_log_state():
+    pass
---
+++ tests/test_handlers/test_errors.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.mark.skip
+async def test_handle_connection_error():
+    pass
+
+
+@pytest.mark.skip
+async def test_handle_message_is_not_modified():
+    pass
+
+
+@pytest.mark.skip
+async def test_handle_error():
+    pass
