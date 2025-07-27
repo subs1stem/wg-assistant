6f6201559df21ae6da2dc00139c2a41c09b9412a
subs1stem <subs1stem@yandex.ru>
2025-07-27 19:44:01 +0300

    WIP: tests for server factory

--- tests/test_servers/test_server_factory.py
+++ tests/test_servers/test_server_factory.py
@@ -2,12 +2,14 @@ from unittest.mock import MagicMock, patch
 
 import pytest
 
-from wg_assistant.models.server import ServerModel, Protocol
+from wg_assistant.models.server import ServerModel, Protocol, ServerType
 from wg_assistant.servers.server_factory import ServerFactory
 from wg_assistant.wireguard.client.local import LocalClient
 from wg_assistant.wireguard.client.remote import RemoteClient
+from wg_assistant.wireguard.linux import Linux
 from wg_assistant.wireguard.protocol.amnezia_wg import AmneziaWGProtocol
 from wg_assistant.wireguard.protocol.wireguard import WireguardProtocol
+from wg_assistant.wireguard.routeros import RouterOS
 
 
 @pytest.mark.parametrize(
@@ -26,10 +28,81 @@ def test_get_protocol(protocol_enum, expected_protocol_class):
 
 def test_get_protocol_unhandled():
     model = ServerModel(name='test')
-    model.__setattr__('protocol', MagicMock(spec=Protocol, value='fake_protocol'))
+    model.protocol = MagicMock(spec=Protocol, value='fake_protocol')
 
     with pytest.raises(ValueError, match='Unhandled protocol type: fake_protocol'):
         ServerFactory._get_protocol(model)
+
+
+@pytest.mark.parametrize(
+    'server_model_type,expected_wireguard_class',
+    [
+        (ServerType.LINUX, Linux),
+        (ServerType.ROUTEROS, RouterOS),
+    ],
+    ids=['linux', 'routeros'],
+)
+@patch('wg_assistant.servers.server_factory.Linux', autospec=True)
+@patch('wg_assistant.servers.server_factory.RouterOS', autospec=True)
+@patch(
+    'wg_assistant.servers.server_factory.ServerFactory._get_linux_client',
+    autospec=True,
+    return_value='mock_linux_client',
+)
+def test_create_instance(
+        mock_get_linux_client,
+        mock_routeros_class,
+        mock_linux_class,
+        server_model_type,
+        expected_wireguard_class,
+):
+    protocol = WireguardProtocol()
+    protocol_enum = Protocol.WIREGUARD
+
+    model = ServerModel(
+        name='test',
+        type=server_model_type,
+        protocol=protocol_enum,
+        path_to_config='/etc/wireguard/wg0.conf',
+        interface_name='wg0',
+        server='wg.example.com',  # type: ignore
+        port=45022,
+        username='root',
+        password='password',
+    )
+
+    result = ServerFactory._create_instance(model, protocol)
+    assert isinstance(result, expected_wireguard_class)
+
+    if server_model_type == ServerType.LINUX:
+        mock_routeros_class.assert_not_called()
+        mock_get_linux_client.assert_called_once_with(model)
+        mock_linux_class.assert_called_once_with(
+            client='mock_linux_client',
+            protocol=protocol,
+            interface_name='wg0',
+            path_to_config='/etc/wireguard/wg0.conf',
+        )
+    else:
+        mock_linux_class.assert_not_called()
+        mock_get_linux_client.assert_not_called()
+        mock_routeros_class.assert_called_once_with(
+            server='wg.example.com',
+            port=45022,
+            username='root',
+            password='password',
+            protocol=protocol,
+            interface_name='wg0',
+        )
+
+
+def test_create_instance_unhandled():
+    model = ServerModel(name='test')
+    model.type = MagicMock(spec=ServerType, value='fake_server')
+    protocol = WireguardProtocol()
+
+    with pytest.raises(ValueError, match='Unhandled server type: fake_server'):
+        ServerFactory._create_instance(model, protocol)
 
 
 @pytest.mark.parametrize(
